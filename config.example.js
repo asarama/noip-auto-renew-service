0fc919cfbdf3a4e68af77924f53b51b99af931be
asarama <arshameslami@hotmail.com>
2020-07-01 21:43:09 -0400

    Added higher level functions for filling input fields

--- config.example.js
+++ config.example.js
@@ -1,3 +1,7 @@
 module.exports = {
-    debug_mode: false
+    debug_mode: false,
+    no_ip: {
+        username: "sample_username",
+        password: "sample_password",
+    }
 }
--- mvp.js
+++ mvp.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 
 const 
+    DEFAULT_TIMEOUT = 5 * 1000,
     noip_urls = {
         login: "https://www.noip.com/login?ref_url=console"
     },
@@ -10,15 +11,19 @@ const
     },
     config = require('./config');
 
-String.prototype.copy = () => {
+String.prototype.repeat = function(num) {
+    return new Array( num + 1 ).join( this );
+}
+
+String.prototype.copy = function() {
     return this.repeat(1)
 }
 
 const pad_string = (input_string, minimum_string_length, padding_character="0", prepend=true) => {
 
-    let return_string = input_string.copy()
+    let return_string = new String(input_string).copy()
 
-    let string_length_delta = minimum_string_length - return_string.length()
+    let string_length_delta = minimum_string_length - return_string.length
 
     while (string_length_delta > 0) {
 
@@ -55,19 +60,11 @@ let browser_launch_options = {
     headless: config.debug_mode ? false : true
 };
 
-(async () => {
-    const browser = await puppeteer.launch(browser_launch_options);
-    const page = await browser.newPage();
-
-    await navigate(page, noip_urls.login);
-    await browser.close();
-})();
-
 let 
     session_timestamp = create_timestamp(),
     navigation_counter = 0;
-let navigate = async (page, url) => {    
-    await page.goto(url);
+let navigate = async (page, url, waitUntil='networkidle2') => {    
+    await page.goto(url, {waitUntil});
 
     if (config.debug_mode) {
         await page.screenshot({path: `./debugging/${session_timestamp}-${navigation_counter}.png`});
@@ -75,3 +72,95 @@ let navigate = async (page, url) => {
     }
     
 };
+
+// Higher level methods
+
+const element_exists = async (page, selector, throw_error=false) => {
+    
+    let wait_for_response
+    
+    try {
+
+        wait_for_response = await page.waitForSelector(selector)
+        return true
+
+    } catch(err) {
+
+        console.error(err)
+
+        if (throw_error) {
+            throw new Error(`Could not find element with selector "${selector}"`)
+        }
+        
+        return false
+        
+    }
+}
+
+const set_element_value = async (page, selector, value, throw_error=false) => {
+    
+    let set_value_response
+    
+    try {
+
+        set_value_response = await page.evaluate((selector, value) => {
+            const element = document.querySelector(selector);
+
+            if (!element) {
+                throw new Error(`Element with selector "${selector}" not found`)
+                
+            }
+
+            element.value = value
+        }, selector, value)
+
+        return true
+
+    } catch(err) {
+
+        console.error(err)
+
+        if (throw_error) {
+            throw new Error(`Could not set value of element with selector "${selector}"`)
+        }
+        
+        return false
+        
+    }
+}
+
+
+const fill_input_field = async (page, selector, value) => {
+
+    // Confirm element exists
+    if (!await element_exists(page, selector)) {
+        throw new Error(`Could not find element to fill with selector "${selector}"`)
+    }
+
+    const set_value_response = await set_element_value(page, selector, value)
+
+    if (!set_value_response) {
+        throw new Error(`Could not set value for element with selector "${selector}"`)
+    }
+
+}
+
+// Main script
+(async () => {
+    const browser = await puppeteer.launch(browser_launch_options);
+    const page = await browser.newPage();
+    
+    // Set default timeout to 5 seconds
+    page.setDefaultTimeout(DEFAULT_TIMEOUT)
+
+    // Navigate to noip login page
+    await navigate(page, noip_urls.login);
+
+    // Fill in username and password
+    await fill_input_field(page, noip_selectors.username, config.no_ip.username)
+    await fill_input_field(page, noip_selectors.password, config.no_ip.password)
+
+
+    await browser.close();
+})();
+
